120fdacb76adbbf61c96669d9e15efad797cc7b1
YoussefHanouch <7anouch99@gmail.com>
2025-10-19 02:46:15 +0100

    Change style and add operations in the calculator

--- calculer2.js
+++ calculer2.js
@@ -1,25 +1,36 @@
-function egale(){
-    let N1=document.getElementById("nb1").value;
-    let N2=document.getElementById("nb2").value;
-    let res=document.getElementById("ress");
- 
-    //recherche operation
-     let addition =document.getElementById("add");
-     let soustr =document.getElementById("sous");
-     let division =document.getElementById("div");
-     if(addition.checked){
-        res.innerHTML=parseFloat(N1)+parseFloat(N2);
-     }
-     if(soustr.checked){
-         res.innerHTML=parseFloat(N1)-parseFloat(N2);
-     }
-     if(division.checked){
-         if(N2!=0){
-             res.innerHTML=parseFloat(N1)/parseFloat(N2)
-         }
-         else{
-             alert("opperation n'est pas coucher");
-         }
-     }
+function egale() {
+    let N1 = parseFloat(document.getElementById("nb1").value);
+    let N2 = parseFloat(document.getElementById("nb2").value);
+    let res = document.getElementById("ress");
 
+    let addition = document.getElementById("add");
+    let soustr = document.getElementById("sous");
+    let multiplication = document.getElementById("mul");
+    let division = document.getElementById("div");
+    let modulo = document.getElementById("mod");
+    let power = document.getElementById("pow");
+
+    if (addition.checked) {
+        res.innerHTML = "Result: " + (N1 + N2);
+    } else if (soustr.checked) {
+        res.innerHTML = "Result: " + (N1 - N2);
+    } else if (multiplication.checked) {
+        res.innerHTML = "Result: " + (N1 * N2);
+    } else if (division.checked) {
+        if (N2 !== 0) {
+            res.innerHTML = "Result: " + (N1 / N2);
+        } else {
+            alert("Division by zero is not allowed!");
+        }
+    } else if (modulo.checked) {
+        if (N2 !== 0) {
+            res.innerHTML = "Result: " + (N1 % N2);
+        } else {
+            alert("Modulo by zero is not allowed!");
+        }
+    } else if (power.checked) {
+        res.innerHTML = "Result: " + Math.pow(N1, N2);
+    } else {
+        alert("Please select an operation!");
+    }
 }
